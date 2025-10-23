ba7e957b360244efded7de4eeaddfdf234c671a9
dotsenkois <dotsenkois@yandex.ru>
2025-10-23 15:58:35 +0700

    Добалвены перевод на русский всех моделей

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Category
+from .models import Topping
+from .models import Wrapper
+from .models import IceCream
 
-# ...и регистрируем её в админке:
-admin.site.register(Category) 
+admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
