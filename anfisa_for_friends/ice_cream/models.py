5e02fd1c42d50ba94be88877be3620e2c2d9071b
dotsenkois <dotsenkois@yandex.ru>
2025-10-23 15:44:37 +0700

    Добалено русское название таблицы Category

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -8,6 +8,10 @@ class Category(PublishedModel):
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
 
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
 
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
