dbc21dcda412d5ea3cc68d782e2c479c2b813513
dotsenkois <dotsenkois@yandex.ru>
2025-10-23 22:46:56 +0700

    Добавлены настройки отображения полей моделей IceCream и Category

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -3,7 +3,43 @@ from django.contrib import admin
 # Register your models here.
 from .models import Category, IceCream, Topping, Wrapper
 
+admin.site.empty_value_display = 'Не задано'
+
 admin.site.register(Topping)
 admin.site.register(Wrapper)
-admin.site.register(IceCream)
-admin.site.register(Category)
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'output_order',
+    )
+    list_editable = (
+        'slug',
+        'output_order',
+    )
+    search_fields = ('title',)
+    list_filter = ('title',)
+    list_display_links = ('title',)
+# Регистрируем кастомное представление админ-зоны
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
