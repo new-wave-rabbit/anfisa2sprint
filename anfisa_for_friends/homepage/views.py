536af822d094afa8addb91cfe9758daed4440404
dotsenkois <dotsenkois@yandex.ru>
2025-10-24 22:48:19 +0700

    Добавлены q-объекты

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from ice_cream.models import IceCream
-
+# Для применения Q-объектов их нужно импортировать:
+from django.db.models import Q
 
 def index(request):
     template_name = 'homepage/index.html'
     # Запрос:
-    ice_cream_list = IceCream.objects.values('id', 'title')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title'
+    ).filter(
+        # Делаем запрос, объединяя два условия
+        # через Q-объекты и оператор AND:
+        Q(is_published=True) & Q(is_on_main=True)
+    )
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
         'ice_cream_list': ice_cream_list,
