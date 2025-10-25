34ff69146b1230e89f6e429cbda607eb1c5c216e
dotsenkois <dotsenkois@yandex.ru>
2025-10-25 20:43:10 +0700

    Добавлен запрос для получения объекта из бд по его id

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from ice_cream.models import IceCream
 
 def ice_cream_detail(request, pk):
-    template = 'ice_cream/detail.html'
-    context = {}
-    return render(request, template, context)
+    template_name = 'ice_cream/detail.html'
+    # Вызываем .get() и в его параметрах указываем условия фильтрации:
+    ice_cream = IceCream.objects.get(pk=pk)
+    context = {
+        'ice_cream': ice_cream,
+    }
+    return render(request, template_name, context)
 
 
 def ice_cream_list(request):
