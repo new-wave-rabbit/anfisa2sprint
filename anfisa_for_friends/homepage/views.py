611c660cd9f38e23bd902cd82efaae7eb270eb06
dotsenkois <dotsenkois@yandex.ru>
2025-10-26 21:34:02 +0700

    Привел в соответствие с требованиями

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,29 +1,22 @@
+# homepage/views.py
+
 from django.shortcuts import render
+
 from ice_cream.models import IceCream
-# Для применения Q-объектов их нужно импортировать:
-from django.db.models import Q
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Запрос:
-    # ice_cream_list = IceCream.objects.values(
-    #     'id', 'title', 'description', 'category'
-    # ).filter(
-    #     # Делаем запрос, объединяя два условия
-    #     # через Q-объекты и оператор AND:
-    #     Q(is_published=True) & Q(is_on_main=True)
-    # ).order_by('title')[1:4]
-    ice_cream_list = IceCream.objects.select_related('category').filter(
-    # Вернуть только те объекты IceCream, у которых
-    # в связанном объекте Category в поле is_published хранится значение True:
-    category__is_published=True
-)
+    # Запрашиваем нужные поля из базы данных:
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+    ).filter(
+        # Проверяем, что
+        is_published=True,  # Сорт разрешён к публикации;
+        is_on_main=True,  # Сорт разрешён к публикации на главной странице;
+        category__is_published=True  # Категория разрешена к публикации.
+    )
 
-    # ice_cream_list = IceCream.objects.values('id', 'title', 'category__title')
-
-    # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
     return render(request, template_name, context)
