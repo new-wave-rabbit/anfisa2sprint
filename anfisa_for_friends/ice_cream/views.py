156dcf6c1f9148c3721627f5f67e8d5c1e535ffe
dotsenkois <dotsenkois@yandex.ru>
2025-10-26 21:37:44 +0700

    Привел в соответсиве ice_cream_detail

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -4,9 +4,7 @@ from ice_cream.models import IceCream
 def ice_cream_detail(request, pk):
     template_name = 'ice_cream/detail.html'
     ice_cream = get_object_or_404(
-        IceCream.objects.values(
-            'title', 'description'
-        ).filter(is_published=True),
+        IceCream.objects.filter(is_published=True, category__is_published=True),
         pk=pk
     )
     context = {
