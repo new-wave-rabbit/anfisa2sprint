d3215bfbc0c7b0c8e562492cb86cfbe4fe39ff7a
dotsenkois <dotsenkois@yandex.ru>
2025-10-26 21:25:29 +0700

    фильтрация связанных моделей

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,13 +6,21 @@ from django.db.models import Q
 def index(request):
     template_name = 'homepage/index.html'
     # Запрос:
-    ice_cream_list = IceCream.objects.values(
-        'id', 'title'
-    ).filter(
-        # Делаем запрос, объединяя два условия
-        # через Q-объекты и оператор AND:
-        Q(is_published=True) & Q(is_on_main=True)
-    ).order_by('title')[1:4]
+    # ice_cream_list = IceCream.objects.values(
+    #     'id', 'title', 'description', 'category'
+    # ).filter(
+    #     # Делаем запрос, объединяя два условия
+    #     # через Q-объекты и оператор AND:
+    #     Q(is_published=True) & Q(is_on_main=True)
+    # ).order_by('title')[1:4]
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+    # Вернуть только те объекты IceCream, у которых
+    # в связанном объекте Category в поле is_published хранится значение True:
+    category__is_published=True
+)
+
+    # ice_cream_list = IceCream.objects.values('id', 'title', 'category__title')
+
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
         'ice_cream_list': ice_cream_list,
